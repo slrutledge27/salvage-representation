0fdf31c8c4d123239d42a231bd211e9874b1ec80
Samantha Rutledge <slrutledge27@gmail.com>
2026-01-22 09:59:41 -0600

    code changes

--- Code/4.seasonality-analyses-and-figures.R
+++ Code/4.seasonality-analyses-and-figures.R
@@ -1,46 +1,76 @@
 library(tidyverse)
 library(ggplot2)
 library(viridis)
+library(lubridate)
 ## split Arctos_dates_preps date column into year + month/day
-Arctos_dates_preps[c('year','month','day')] <- str_split_fixed(Arctos_dates_preps$date, '-', 3)
+#Arctos_dates_preps[c('year','month','day')] <- str_split_fixed(Arctos_dates_preps$date, '-', 3)
+#Arctos_dates_preps <- Arctos_dates_preps %>% unite(day_month, day, month, sep = "-")
 
-## replace month numbers with letters
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "01", "Jan")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "02", "Feb")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "03", "Mar")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "04", "Apr")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "05", "May")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "06", "Jun")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "07", "Jul")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "08", "Aug")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "09", "Sep")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "10", "Oct")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "11", "Nov")
-Arctos_dates_preps$month <- replace(Arctos_dates_preps$month, Arctos_dates_preps$month == "12", "Dec")
+## convert between date formats
+Arctos_dates_preps$mdy_format <- format(Arctos_dates_preps$date, "%m-%d-%Y")
+## extract month
+Arctos_dates_preps$month <- month(ymd(Arctos_dates_preps$date))
 
-Arctos_dates_preps <- Arctos_dates_preps %>% unite(day_month, day, month, sep = "-")
+Arctos_month_grouped <- Arctos_dates_preps %>% group_by(coll_method, month) %>% summarize(count = n())
 
 ### split into active and salvage
 df_salvage <- Arctos_dates_preps%>% filter(coll_method == "salvage")
 df_active <- Arctos_dates_preps%>% filter(coll_method == "active")
 
+## calculate proportions
+month_counts_salvage <- table(df_salvage$month)
+prop_month_salvage <- as.data.frame(prop.table(month_counts_salvage))
+
+month_counts_active <- table(df_active$month)
+prop_month_active <- as.data.frame(prop.table(month_counts_active))
+
+## bin dates by month
+#month_binned_Arctos <- Arctos_dates_preps %>% 
+  #group_by(coll_method, month) %>% summarize(count = n())
+
 ## get frequencies of date
-date_counts_salvage<- as.data.frame(table(df_salvage$day_month))
-date_counts_active<- as.data.frame(table(df_active$day_month))
+#date_counts_salvage<- as.data.frame(table(df_salvage$day_month))
+#date_counts_active<- as.data.frame(table(df_active$day_month))
 
 ## rename columns
-names(date_counts_salvage) <- c("day_month", "count")
-names(date_counts_active) <- c("day_month", "count")
+#names(date_counts_salvage) <- c("day_month", "count")
+#names(date_counts_active) <- c("day_month", "count")
 
 ## re-add collecting method
-date_counts_salvage <- date_counts_salvage %>% mutate(coll_method = "salvage")
-date_counts_active <- date_counts_active %>% mutate(coll_method = "active")
+#date_counts_salvage <- date_counts_salvage %>% mutate(coll_method = "salvage")
+#date_counts_active <- date_counts_active %>% mutate(coll_method = "active")
 
-date_counts <- rbind(date_counts_salvage, date_counts_active)
+#date_counts <- rbind(date_counts_salvage, date_counts_active)
+
+## add month names for graphing
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 1, "1-Jan")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 2, "2-Feb")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 3, "3-Mar")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 4, "4-Apr")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 5, "5-May")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 6, "6-Jun")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 7, "7-Jul")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 8, "8-AUg")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 9, "9-Sep")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 10, "10-Oct")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 11, "11-Nov")
+Arctos_month_grouped$month <- replace(Arctos_month_grouped$month, Arctos_month_grouped$month == 12, "12-Dec")
 
 ## create polar bar chart
-Arctos_date_polar_chart_dm <- ggplot(date_counts, aes(x = day_month, y = count, fill = coll_method)) +
-  geom_bar(stat = "identity") +
-  coord_polar(start = 0) +
-  theme_minimal()
+Arctos_date_polar_chart_dm <- ggplot(Arctos_month_grouped, aes(x = month, y = count, fill = coll_method)) +
+  geom_bar(position="fill", stat = "identity") +
+  coord_polar(start = 0) +  scale_fill_viridis_d(option = "viridis") +
+  xlab("Collecting Method") +
+  ylab("Proportion of Specimens") + theme_minimal()
+ 
 Arctos_date_polar_chart_dm
+
+
+Arctos_date_polar_chart <- ggplot(Arctos_month_grouped, aes(x = month, y = count, fill = coll_method)) +
+  geom_bar(position="stack", stat = "identity") +
+  coord_polar(start = 0) +  scale_fill_viridis_d(option = "viridis") +
+  xlab("Collecting Method") +
+  ylab("Specimen Count") + theme_minimal()
+
+Arctos_date_polar_chart
+
--- Code/5.prep-types-analyses-and-figures.R
+++ Code/5.prep-types-analyses-and-figures.R
@@ -1,5 +1,9 @@
 ### get proportions
 Arctos_dates_preps
+## group by coll method and prep type
+Arctos_prep_grouped <- Arctos_dates_preps %>% group_by(coll_method, PREP) %>% summarize(count = n())
+
+## get proportions
 df_salvage <- Arctos_dates_preps%>% filter(coll_method == "salvage")
 df_active <- Arctos_dates_preps%>% filter(coll_method == "active")
 
@@ -9,10 +13,20 @@ prop_preps_salvage <- as.data.frame(prop.table(prep_counts_salvage))
 prep_counts_active <- table(df_active$PREP)
 prop_preps_active <- as.data.frame(prop.table(prep_counts_active))
 
-df_skin <- Arctos_dates_preps%>% filter(PREP == "skin")
-df_skel <- Arctos_dates_preps %>% filter(PREP == "skel")
-df_fluid <- Arctos_dates_preps %>% filter(PREP == "fluid")
-df_partial <-Arctos_dates_preps %>% filter(PREP == "partial")
+### build figures
+## raw counts
+ggplot(Arctos_prep_grouped, aes(fill=PREP, y=count, x=coll_method)) + 
+  geom_bar(position="stack", stat="identity") + scale_fill_viridis_d(option = "viridis") +  
+  xlab("Collecting Method") +
+  ylab("Specimen Count") + theme_minimal()
+
+## proportion
+ggplot(Arctos_prep_grouped, aes(fill=PREP, y=count, x=coll_method)) + 
+  geom_bar(position="fill", stat="identity") + scale_fill_viridis_d(option = "viridis") +
+  xlab("Collecting Method") +
+  ylab("Proportion of Specimens") + theme_minimal()
+
+
 
 ## get counts
 df_skin <- Arctos_dates_preps%>% filter(PREP == "skin")
